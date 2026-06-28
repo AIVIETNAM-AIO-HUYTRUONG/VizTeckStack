4cb5f694352763cc1ed5d8d437d65c72afe2fa0d
INTJBoy <intjboy222k@gmail.com>
2026-06-29 00:01:36 +0700

    fix: restore prisma wildcard export to fix svc-roadmap build

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -9,3 +9,5 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 export const db: PrismaClient = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+
+export * from '@prisma/client';
